0afef913b50a1a09722b27c131eb877a8e26cdee
SeraF-PC\SeraF <com.gmail@seraf.babiarz.krzysztof>
2018-03-23 18:00:25 +0000

    app number 16 part 2 need to add notes and change if statement to switch statement

--- AppNumber16/app/src/main/java/com/example/seraf/babiarz/krzysztof/appnumber16/MainActivity.java
+++ AppNumber16/app/src/main/java/com/example/seraf/babiarz/krzysztof/appnumber16/MainActivity.java
@@ -23,25 +23,59 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TextView txtDice1 = (TextView) findViewById(R.id.txtDice1);
-        TextView txtDice2 = (TextView) findViewById(R.id.txtDice2);
-        TextView txtDice3 = (TextView) findViewById(R.id.txtDice3);
-        TextView txtDice4 = (TextView) findViewById(R.id.txtDice4);
-        TextView txtDice5 = (TextView) findViewById(R.id.txtDice5);
-        TextView txtDice6 = (TextView) findViewById(R.id.txtDice6);
+        final TextView txtDice1 = (TextView) findViewById(R.id.txtDice1);
+        final TextView txtDice2 = (TextView) findViewById(R.id.txtDice2);
+        final TextView txtDice3 = (TextView) findViewById(R.id.txtDice3);
+        final TextView txtDice4 = (TextView) findViewById(R.id.txtDice4);
+        final TextView txtDice5 = (TextView) findViewById(R.id.txtDice5);
+        final TextView txtDice6 = (TextView) findViewById(R.id.txtDice6);
 
-        /*
+
         ImageView imgDice1 = (ImageView) findViewById(R.id.imgDice1);
         ImageView imgDice2 = (ImageView) findViewById(R.id.imgDice2);
         ImageView imgDice3 = (ImageView) findViewById(R.id.imgDice3);
         ImageView imgDice4 = (ImageView) findViewById(R.id.imgDice4);
         ImageView imgDice5 = (ImageView) findViewById(R.id.imgDice5);
         ImageView imgDice6 = (ImageView) findViewById(R.id.imgDice6);
-        */
+
 
         Button btnRollDice = (Button) findViewById(R.id.btnRollDice);
 
-        SecureRandom secRand = new SecureRandom();
+        final SecureRandom secRand = new SecureRandom();
+
+        final int[] occurence = new int[7];
+
+        btnRollDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                for(int times = 0; times < 500000; times++){
+
+                    int rendNum = 1 + secRand.nextInt(6);
+                    ++occurence[rendNum];
+
+                }
+
+                for (int faceNum = 1; faceNum < occurence.length; faceNum++){
+
+                    if (faceNum == 1){
+                        txtDice1.setText(occurence[faceNum] + "");
+                    }else if (faceNum == 2){
+                        txtDice2.setText(occurence[faceNum] + "");
+                    }else if (faceNum == 3){
+                        txtDice3.setText(occurence[faceNum] + "");
+                    }else if (faceNum == 4){
+                        txtDice4.setText(occurence[faceNum] + "");
+                    }else if (faceNum == 5){
+                        txtDice5.setText(occurence[faceNum] + "");
+                    }else if (faceNum == 6){
+                        txtDice6.setText(occurence[faceNum] + "");
+                    }
+
+                }
+
+            }
+        });
 
 
     }
