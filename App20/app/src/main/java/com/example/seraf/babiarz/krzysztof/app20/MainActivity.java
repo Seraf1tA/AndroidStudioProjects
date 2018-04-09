6517182be4c6073b0a7c39608853c70dcaad1c60
SeraF-PC\SeraF <com.gmail@seraf.babiarz.krzysztof>
2018-04-09 17:29:27 +0100

    app20 enchanced for loop, modyfying array

--- App20/app/src/main/java/com/example/seraf/babiarz/krzysztof/app20/MainActivity.java
+++ App20/app/src/main/java/com/example/seraf/babiarz/krzysztof/app20/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtModifyElement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +21,62 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        TextView txtOriginalValuesOfTheArray = (TextView) findViewById(R.id.txtOriginalValuesOfTheArray);
+        TextView txtValuesAfterModification = (TextView) findViewById(R.id.txtValuesAfterModification);
+        TextView txtOriginalElement = (TextView) findViewById(R.id.txtOriginalElement);
+        txtModifyElement = (TextView) findViewById(R.id.txtModifyElement);
+        TextView txtValueOfArrayElementAfterModification = (TextView) findViewById(R.id.txtValueOfArrayElementAfterModification);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        String[] stringArray = {"A\n", "B\n", "C\n", "D\n", "E\n", "F\n", "G\n", "H\n", "I\n", "J\n", "K\n",
+                "L\n", "M\n", "N\n", "O\n", "P\n", "Q\n", "R\n", "S\n", "T\n", "U\n", "V\n", "W\n",
+                "X\n", "Y\n", "Z\n" };
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        String totalOriginalValues = "";
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        /*enchance for loop: type String name value, witch go trough stringArray array and
+        * it will assign all values to totalOriginalValues*/
+        for (String value : stringArray){
+            totalOriginalValues += value;
+            //totalOriginalValues = totalOriginalValues + value;
         }
 
-        return super.onOptionsItemSelected(item);
+        txtOriginalValuesOfTheArray.setText(totalOriginalValues);
+
+        // Now we modify array
+
+        letsModifyArray(stringArray);
+
+        String totalModifiedValues = "";
+
+        for (String value : stringArray){
+            totalModifiedValues += value;
+        }
+
+        txtValuesAfterModification.setText(totalModifiedValues);
+
+        txtOriginalElement.setText(stringArray[5]);
+
+        letsModifyElement(stringArray[5]);
+
+        txtValueOfArrayElementAfterModification.setText(stringArray[5]);
+
     }
+
+    /*this change element and it will never read as it was from start*/
+    public void letsModifyArray(String[] array){
+
+        for (int i = 0; i < array.length; i++){
+            array[i] = "$" + array[i];
+        }
+
+    }
+
+    /*this create coppy so original in array is not changed*/
+    public void letsModifyElement(String element){
+
+        element = "@#" + element;
+        txtModifyElement.setText(element);
+
+    }
+
 }
