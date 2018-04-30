5505fc97960a3193229389a3eb2eafda6703bc1e
SeraF-PC\SeraF <com.gmail@seraf.babiarz.krzysztof>
2018-04-30 10:16:02 +0100

    app number 23 finish

--- App23/app/src/main/java/com/example/seraf/babiarz/krzysztof/app23/Game.java
+++ App23/app/src/main/java/com/example/seraf/babiarz/krzysztof/app23/Game.java
@@ -1,10 +1,14 @@
 package com.example.seraf.babiarz.krzysztof.app23;
 
+import android.widget.TextView;
+
 public class Game {
 
+    /* Declaration without instantiation */
     private String gameName;
     private int[][] scores;
 
+    /*Creating constractor and instantiating, name of constructor must be the same as class name */
     public Game(String gameName, int[][] scores){
 
         this.gameName = gameName;
@@ -20,15 +24,30 @@ public class Game {
         this.gameName = gameName;
     }
 
+    /*
+
+    * Method with access modifier public
+    * return type int beacuse it need to return int
+    * getMinimumScore name
+    * and this method dont accept any argument( empty brackets )
+
+    */
     public int getMinimumScore(){
 
-        /*creating variable of type int name it lowestScore go in to index 0 and sub 0*/
+        /*
+        * creating variable of type int name it lowestScore go in to index 0 and sub 0
+        * and lowestScore is refering to instance variable scores
+        */
         int lowestScore = scores[0][0]; // assume this value is the smallest score value
 
-        /* enchanced for loop with array of type int named gameScores and it will go trough scores array*/
+        /* enhanced for loop with array of type int named gameScores and it will go trough scores array*/
         for (int[] gameScores : scores){
 
-            /* and another for loop */
+            /*
+            * and another enhanced for loop inside first for loop
+            * int variable, name score and it will iterate(go/repeat) through array gameScores
+            *
+            */
             for (int score : gameScores){
                 if (score < lowestScore){
                     lowestScore = score;
@@ -36,5 +55,49 @@ public class Game {
             }
         }
         return lowestScore;
+    }
+
+    public int getMaximumScore(){
+
+        int highestScore = scores[0][0]; // Assume this is the Highest Score Value
+
+        for (int[] gameScores : scores){
+            for (int score : gameScores){
+                if (score > highestScore){
+                    highestScore = score;
+                }
+            }
+        }
+        return highestScore;
+    }
+
+    public double getTheAverageScores(int[] scores){
+
+        int total = 0;
+
+        // Sum scores for each game
+        for (int score : scores){
+            total+= score;
+        }
+
+        // casting int total to double
+        return (double) total / scores.length;
+    }
+
+    public void letsOutPutToScreen(TextView textView){
+
+        String oldTextViewValue;
+
+        for (int gameIndex = 0; gameIndex < scores.length; gameIndex++){
+
+            for (int gameScore : scores[gameIndex]){
+
+                oldTextViewValue = textView.getText().toString();
+
+                textView.setText(String.format(oldTextViewValue + " Game Number %2d: %3d\n\n\n",
+                        (gameIndex + 1), gameScore));
+            }
+
+        }
     }
 }
--- App23/app/src/main/java/com/example/seraf/babiarz/krzysztof/app23/MainActivity.java
+++ App23/app/src/main/java/com/example/seraf/babiarz/krzysztof/app23/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView txtGameName = (TextView) findViewById(R.id.txtGameName);
+        TextView txtGameScores = (TextView) findViewById(R.id.txtGameScores);
+        TextView txtLowestScore = (TextView) findViewById(R.id.txtLowestScore);
+        TextView txtHighestScore = (TextView) findViewById(R.id.txtHighestScore);
+        TextView txtAverageScore = (TextView) findViewById(R.id.txtAverageScore);
+
+
+        int[][] gameScoresArray = {{45, 67, 34},
+                {23, 56, 49},
+                {23, 69, 88},
+                {17, 28, 84},
+                {38, 54, 75},
+                {51, 34, 71},
+                {15, 83, 46},
+                {36, 47, 15},
+                {83, 94, 34},
+                {17, 37, 0}
+        };
+
+        /*
+        * Creating new object and instantiate
+        * constructor in Game class is called and he take two arguments
+        * String gameName and two-dimentional array type int
+        * with is created here
+        */
+        Game myGame = new Game("God of War", gameScoresArray);
+
+        /*
+        * get and set gameName
+        */
+        txtGameName.setText(myGame.getGameName());
+
+        myGame.letsOutPutToScreen(txtGameScores);
+        txtHighestScore.setText(myGame.getMaximumScore() + "");
+        txtLowestScore.setText(myGame.getMinimumScore() + "");
+
+        String oldTxtAverageValueOfScores;
+
+        for (int gameIndex = 0; gameIndex< gameScoresArray.length; gameIndex++){
+
+            oldTxtAverageValueOfScores = txtAverageScore.getText().toString() + gameIndex + " - ";
+
+            double averageValue = myGame.getTheAverageScores(gameScoresArray[gameIndex]);
+            txtAverageScore.setText(oldTxtAverageValueOfScores + averageValue + "\n");
+
+        }
 
     }
 
