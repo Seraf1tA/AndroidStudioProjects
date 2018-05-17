4ac422b17227ddb7886cf8982e6021cdd6d6eb8c
SeraF-PC\SeraF <com.gmail@seraf.babiarz.krzysztof>
2018-05-17 14:33:37 +0100

    nowy planer, tableLayout

--- Planer/app/src/main/java/com/example/seraf/babiarz/krzysztof/planer/MainActivity.java
+++ Planer/app/src/main/java/com/example/seraf/babiarz/krzysztof/planer/MainActivity.java
@@ -1,26 +1,46 @@
 package com.example.seraf.babiarz.krzysztof.planer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
 
     TextView txtDate;
+
+    //TextView txtMon, txtTue, txtWed, txtThu, txtFri, txtSat, txtSun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtDate = (TextView) findViewById(R.id.txtDate);
+        ArrayList<String> arrayList = new ArrayList<String>();
 
-        Calendar calendar = Calendar.getInstance();
-        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
-        txtDate.setText(currentDate);
+        //txtDate = (TextView) findViewById(R.id.txtDate);
+
+        //Calendar calendar = Calendar.getInstance();
+        //String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+        //txtDate.setText(currentDate);
+
 
     }
+
+
 }
+
